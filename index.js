4c2926faca4acf232f96d7b794719076844f222d
Ryan DiSalle <ryandisalle@yahoo.com>
2020-05-07 18:28:00 -0400

    final

---
+++ index.js
@@ -0,0 +1,8 @@
+$(".jumper").on("click", function(currentTarget) {
+    
+    currentTarget.preventDefault();
+
+    $("body, html").animate({ 
+        scrollTop: $($(this).attr('href')).offset().top 
+    }, 500);
+});
